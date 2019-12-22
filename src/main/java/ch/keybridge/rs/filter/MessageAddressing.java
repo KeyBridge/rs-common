bd897ea17af5a3dc31be34ac0c22d2631f9685d8
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-12-22 01:28:51 -0500

    add retention time annotation

--- src/main/java/ch/keybridge/rs/filter/MessageAddressing.java
+++ src/main/java/ch/keybridge/rs/filter/MessageAddressing.java
@@ -19,6 +19,8 @@
 package ch.keybridge.rs.filter;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.ws.rs.NameBinding;
 
@@ -46,6 +48,7 @@ import javax.ws.rs.NameBinding;
  */
 @NameBinding
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface MessageAddressing {
 
 }
