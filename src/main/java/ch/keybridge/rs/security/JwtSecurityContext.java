f9a2b1773fa8ad712b4850955c2a4da80c8c0eae
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2020-09-14 18:57:38 -0400

    rewrite with fields

--- src/main/java/ch/keybridge/rs/security/JwtSecurityContext.java
+++ src/main/java/ch/keybridge/rs/security/JwtSecurityContext.java
@@ -18,8 +18,6 @@
  */
 package ch.keybridge.rs.security;
 
-import ch.keybridge.rs.type.AuthorizationType;
-import java.security.Principal;
 import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.Collections;
@@ -42,138 +40,17 @@ import javax.ws.rs.core.SecurityContext;
 public class JwtSecurityContext extends AbstractTokenSecurityContext {
 
   /**
-   * 4.1.7. "jti" (JWT ID) Claim The "jti" (JWT ID) claim provides a unique
-   * identifier for the JWT. The identifier value MUST be assigned in a manner
-   * that ensures that there is a negligible probability that the same value
-   * will be accidentally assigned to a different data object; if the
-   * application uses multiple issuers, collisions MUST be prevented among
-   * values produced by different issuers as well. The "jti" claim can be used
-   * to prevent the JWT from being replayed. The "jti" value is a case-
-   * sensitive string. Use of this claim is OPTIONAL.
-   */
-  private final String jti;
-  /**
-   * 4.1.5. "nbf" (Not Before) Claim The "nbf" (not before) claim identifies the
-   * time before which the JWT MUST NOT be accepted for processing. The
-   * processing of the "nbf" claim requires that the current date/time MUST be
-   * after or equal to the not-before date/time listed in the "nbf" claim.
-   * Implementers MAY provide for some small leeway, usually no more than a few
-   * minutes, to account for clock skew. Its value MUST be a number containing a
-   * NumericDate value. Use of this claim is OPTIONAL.
-   */
-  private final ZonedDateTime notBefore;
-  /**
-   * 4.1.4. "exp" (Expiration Time) Claim The "exp" (expiration time) claim
-   * identifies the expiration time on or after which the JWT MUST NOT be
-   * accepted for processing. The processing of the "exp" claim requires that
-   * the current date/time MUST be before the expiration date/time listed in the
-   * "exp" claim. Implementers MAY provide for some small leeway, usually no
-   * more than a few minutes, to account for clock skew. Its value MUST be a
-   * number containing a NumericDate value. Use of this claim is OPTIONAL.
-   */
-  private final ZonedDateTime notAfter;
-
-  /**
-   * The maximum number of times the issued token may be refreshed.
-   */
-  private final int refreshLimit;
-  /**
-   * The current number of times this token has been refreshed.
-   */
-  private final int refreshCount;
-
-  public JwtSecurityContext(boolean secure,
-                            String userId,
-                            Collection<String> scope,
-                            String jti,
-                            ZonedDateTime notBefore,
-                            ZonedDateTime notAfter,
-                            int refreshCount,
-                            int refreshLimit) {
-    super(userId, scope, secure);
-    this.jti = jti;
-    this.notBefore = notBefore;
-    this.notAfter = notAfter;
-    this.refreshCount = refreshCount;
-    this.refreshLimit = refreshLimit;
-  }
-
-  //<editor-fold defaultstate="collapsed" desc="Getters">
-  public AuthorizationType getScheme() {
-    return scheme;
-  }
-
-  public Collection<String> getScope() {
-    return scope;
-  }
-
-  public String getJti() {
-    return jti;
-  }
-
-  public ZonedDateTime getNotBefore() {
-    return notBefore;
-  }
-
-  public ZonedDateTime getNotAfter() {
-    return notAfter;
-  }
-
-  public int getRefreshCount() {
-    return refreshCount;
-  }
-
-  public int getRefreshLimit() {
-    return refreshLimit;
-  }//</editor-fold>
-
-  /**
-   * Check if the authentication token is eligible for refreshment.
-   *
-   * @return TRUE if the authentication token is eligible for refreshment
-   */
-  public boolean isEligibleForRefreshment() {
-    if (notBefore != null && notBefore.isBefore(ZonedDateTime.now())) {
-      return false;
-    }
-    if (notAfter != null && notAfter.isAfter(ZonedDateTime.now())) {
-      return false;
-    }
-    return refreshCount < refreshLimit;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isSecure() {
-    return secure;
-  }
-
-  /**
-   * {@inheritDoc} The current authenticated user is identified by the user
-   * record UID value, which may be a user or, more commonly, the user's API
+   * Construct a new JWT security context. The current authenticated user is
+   * identified by the JWT ID ("jti") value, which references the user's API
    * authorization key.
+   *
+   * @param principal A JwtPrincipal instance containing the conveyed JWT values
+   * @param scope     The list of scopes associated with the current user;
+   * @param secure    Whether this request was made using a secure channel, such
+   *                  as HTTPS.
    */
-  @Override
-  public Principal getUserPrincipal() {
-    return () -> userId;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isUserInRole(String role) {
-    return scope.contains(role);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public String getAuthenticationScheme() {
-    return scheme.name();
+  public JwtSecurityContext(JwtPrincipal principal, Collection<String> scope, boolean secure) {
+    super(principal, scope, secure);
   }
 
   /**
@@ -182,22 +59,88 @@ public class JwtSecurityContext extends AbstractTokenSecurityContext {
    */
   public static class Builder {
 
+    /**
+     * A boolean indicating whether the instant request was made using a secure
+     * channel, such as HTTPS.
+     */
     private boolean secure;
-    private String userId;
-    private Collection<String> scope;
-    private String jti;
-    private ZonedDateTime notBefore;
-    private ZonedDateTime notAfter;
-    private int refreshCount;
-    private int refreshLimit;
+    /**
+     * The token owner's group privileges.
+     */
+    private Collection<String> scope;  // required
+
+    /**
+     * 4.1.1. "iss" (Issuer) Claim The "iss" (issuer) claim identifies the
+     * principal that issued the JWT. The processing of this claim is generally
+     * application specific. The "iss" value is a case-sensitive string
+     * containing a StringOrURI value. Use of this claim is OPTIONAL.
+     */
+    private String issuer;
+    /**
+     * 4.1.2. "sub" (Subject) Claim The "sub" (subject) claim identifies the
+     * principal that is the subject of the JWT. The claims in a JWT are
+     * normally statements about the subject. The subject value MUST either be
+     * scoped to be locally unique in the context of the issuer or be globally
+     * unique. The processing of this claim is generally application specific.
+     * The "sub" value is a case-sensitive string containing a StringOrURI
+     * value. Use of this claim is OPTIONAL.
+     */
+    private String subject; // required
+    /**
+     * 4.1.3. "aud" (Audience) Claim The "aud" (audience) claim identifies the
+     * recipients that the JWT is intended for. Each principal intended to
+     * process the JWT MUST identify itself with a value in the audience claim.
+     * If the principal processing the claim does not identify itself with a
+     * value in the "aud" claim when this claim is present, then the JWT MUST be
+     * rejected. In the general case, the "aud" value is an array of case-
+     * sensitive strings, each containing a StringOrURI value. In the special
+     * case when the JWT has one audience, the "aud" value MAY be a single
+     * case-sensitive string containing a StringOrURI value. The interpretation
+     * of audience values is generally application specific. Use of this claim
+     * is OPTIONAL.
+     */
+    private String audience;
+    /**
+     * 4.1.4. "exp" (Expiration Time) Claim The "exp" (expiration time) claim
+     * identifies the expiration time on or after which the JWT MUST NOT be
+     * accepted for processing. The processing of the "exp" claim requires that
+     * the current date/time MUST be before the expiration date/time listed in
+     * the "exp" claim. Implementers MAY provide for some small leeway, usually
+     * no more than a few minutes, to account for clock skew. Its value MUST be
+     * a number containing a NumericDate value. Use of this claim is OPTIONAL.
+     */
+    private ZonedDateTime expirationTime;
+    /**
+     * 4.1.5. "nbf" (Not Before) Claim The "nbf" (not before) claim identifies
+     * the time before which the JWT MUST NOT be accepted for processing. The
+     * processing of the "nbf" claim requires that the current date/time MUST be
+     * after or equal to the not-before date/time listed in the "nbf" claim.
+     * Implementers MAY provide for some small leeway, usually no more than a
+     * few minutes, to account for clock skew. Its value MUST be a number
+     * containing a NumericDate value. Use of this claim is OPTIONAL.
+     */
+    private ZonedDateTime notBefore;  // required
+    /**
+     * 4.1.6. "iat" (Issued At) Claim The "iat" (issued at) claim identifies the
+     * time at which the JWT was issued. This claim can be used to determine the
+     * age of the JWT. Its value MUST be a number containing a NumericDate
+     * value. Use of this claim is OPTIONAL.
+     */
+    private ZonedDateTime issuedAt;
+    /**
+     * 4.1.7. "jti" (JWT ID) Claim The "jti" (JWT ID) claim provides a unique
+     * identifier for the JWT. The identifier value MUST be assigned in a manner
+     * that ensures that there is a negligible probability that the same value
+     * will be accidentally assigned to a different data object; if the
+     * application uses multiple issuers, collisions MUST be prevented among
+     * values produced by different issuers as well. The "jti" claim can be used
+     * to prevent the JWT from being replayed. The "jti" value is a case-
+     * sensitive string. Use of this claim is OPTIONAL.
+     */
+    private String jwtId;  // required
 
     public Builder withSecure(boolean secure) {
       this.secure = secure;
-      return this;
-    }
-
-    public Builder withUserId(String userId) {
-      this.userId = userId;
       return this;
     }
 
@@ -206,8 +149,23 @@ public class JwtSecurityContext extends AbstractTokenSecurityContext {
       return this;
     }
 
-    public Builder withJti(String jti) {
-      this.jti = jti;
+    public Builder withIssuer(String issuer) {
+      this.issuer = issuer;
+      return this;
+    }
+
+    public Builder withSubject(String subject) {
+      this.subject = subject;
+      return this;
+    }
+
+    public Builder withAudience(String audience) {
+      this.audience = audience;
+      return this;
+    }
+
+    public Builder withExpirationTime(ZonedDateTime expirationTime) {
+      this.expirationTime = expirationTime;
       return this;
     }
 
@@ -216,18 +174,13 @@ public class JwtSecurityContext extends AbstractTokenSecurityContext {
       return this;
     }
 
-    public Builder withNotAfter(ZonedDateTime notAfter) {
-      this.notAfter = notAfter;
+    public Builder withIssuedAt(ZonedDateTime issuedAt) {
+      this.issuedAt = issuedAt;
       return this;
     }
 
-    public Builder withRefreshCount(int refreshCount) {
-      this.refreshCount = refreshCount;
-      return this;
-    }
-
-    public Builder withRefreshLimit(int refreshLimit) {
-      this.refreshLimit = refreshLimit;
+    public Builder withJwtId(String jwtId) {
+      this.jwtId = jwtId;
       return this;
     }
 
@@ -237,8 +190,8 @@ public class JwtSecurityContext extends AbstractTokenSecurityContext {
      * @return a complete JwtSecurityContext instance
      */
     public JwtSecurityContext build() {
-      if (userId == null) {
-        throw new IllegalArgumentException("userId is required");
+      if (jwtId == null) {
+        throw new IllegalArgumentException("jwtId is required");
       }
       if (scope == null) {
         throw new IllegalArgumentException("scope is required");
@@ -246,13 +199,13 @@ public class JwtSecurityContext extends AbstractTokenSecurityContext {
       if (notBefore == null) {
         throw new IllegalArgumentException("notBefore is required");
       }
-      if (notAfter == null) {
-        throw new IllegalArgumentException("notAfter is required");
+      if (subject == null) {
+        throw new IllegalArgumentException("subject is required");
       }
-      if (jti == null) {
-        throw new IllegalArgumentException("jti is required");
-      }
-      return new JwtSecurityContext(secure, userId, scope, jti, notBefore, notAfter, refreshCount, refreshLimit);
+
+      return new JwtSecurityContext(new JwtPrincipal(issuer, subject, audience, expirationTime, notBefore, issuedAt, jwtId),
+                                    scope,
+                                    secure);
     }
 
   }
